8c875466bcfebb9723d96ed147721166b841ccbd
Gabe Amaya <gabeamaya2@gmail.com>
2023-01-09 13:42:23 -0600

    desktop update

--- src/components/FeaturedProjects.vue
+++ src/components/FeaturedProjects.vue
@@ -2,13 +2,14 @@
     <section id="featured">
         <div class="flex flex-col gap-2 lg:gap-2">
             <h3>Featured Projects</h3>
-            <p>Check out some of my favorite projects.</p>
-            <div class="grid grid-cols-1 gap-7 pt-8 mx-auto">
+            <p class="text-sm lg:text-lg slg:leading-8 leading-relaxed">Check out some of my favorite projects.</p>
+            <div class="grid grid-cols-1 lg:grid-cols-2 gap-7 pt-8 mx-auto">
                 <a href="https://gabes-shopping-list.netlify.app" class="flex flex-col w-full p-6 py-6 lg:p-6 lg:py-10 items-center gap-8 rounded-sm transition-colors border-[1px] bg-zinc-900 border-light" target="_blank">
                     <img src="../assets/shopping-app.png" alt="Shopping App" class="w-full object-cover rounded-sm">
                     <div class="flex-flex-col justify-between gap-4">
                         <h3 class="mb-3">Vue Shopping List App</h3>
                         <p class="text-zinc-400">A small shopping list app that showcases my knowledge of the basics of Vue.</p>
+                        
                     </div>
                 </a>
                 <a href="https://sombruh.webflow.io/" class="flex flex-col w-full p-6 py-6 lg:p-6 lg:py-10 items-center gap-8 rounded-sm transition-colors border-[1px] bg-zinc-900 border-light" target="_blank">
--- src/views/Projects.vue
+++ src/views/Projects.vue
@@ -3,7 +3,7 @@
     <div class="w-full max-w-screen-lg mx-auto pb-24 sm:px-16 slg:px-24 lg:mt-48 lg:mt-58 px-4">
       <div class="flex flex-col gap-8 w-full lg:gap-12">
         <h1>My Projects</h1>
-        <p>Full list of my most recent projects showcasing my skills in Development and Design.</p>
+        <p class="text-sm lg:text-lg slg:leading-8 leading-relaxed">Full list of my most recent projects showcasing my skills in Development and Design.</p>
         <hr class="border-0 opacity-25 h-[4px]">
         <!-- Projects container -->
         <div class="flex flex-col items-center justify-center gap-28">
@@ -14,7 +14,7 @@
             <!-- Project content -->
             <div class="flex flex-col items-left gap-6">
               <h3>Vue Shopping App</h3>
-              <p>A simple shopping app built with Vue JS. I learned the basics on how to filter through json files data to load in content along with components, router and the fundamentals of Options API.</p>
+              <p class="text-sm lg:text-lg slg:leading-8 leading-relaxed">A simple shopping app built with Vue JS. I learned the basics on how to filter through json files data to load in content along with components, router and the fundamentals of Options API.</p>
               <a href="https://gabes-shopping-list.netlify.app" class="px-2 py-2 rounded-md transition-colors border-[1px] w-fit text-sm md:px-4 slg:px-6 bg-teal-500 border-black hover:bg-teal-700">View App</a>
             </div>
           </div>
@@ -25,7 +25,7 @@
             <!-- Project content -->
             <div class="flex flex-col items-left gap-6">
               <h3>Zarya Creative Studio</h3>
-              <p>A beautiful website crafted by me in Webflow.  Main goal was to really flex my design skills and think outside of the norm. This agency is for small business' to hire my friend and I for creative services at an affordable rate.</p>
+              <p class="text-sm lg:text-lg slg:leading-8 leading-relaxed">A beautiful website crafted by me in Webflow.  Main goal was to really flex my design skills and think outside of the norm. This agency is for small business' to hire my friend and I for creative services at an affordable rate.</p>
               <a href="https://gabes-shopping-list.netlify.app" class="px-2 py-2 rounded-md transition-colors border-[1px] w-fit text-sm md:px-4 slg:px-6 bg-teal-500 border-black hover:bg-teal-700">View Site</a>
             </div>
           </div>
